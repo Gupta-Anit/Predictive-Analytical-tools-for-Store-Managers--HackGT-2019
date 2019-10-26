81f77b16669ddb8e1f5a48e939ff686f4ff6d258
anit2 <anitg.4@gmail.com>
2019-10-26 13:11:44 -0400

    Update get_other_frequent_Item_improved.py

--- get_other_frequent_Item_improved.py
+++ get_other_frequent_Item_improved.py
@@ -1,13 +1,21 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
+import pandas as pd
 dfassoc = pd.read_excel('assoc_rules.xlsx')
 def other_frequently_bought_item(item):
-    print(dfassoc.loc[rules['antecedents'] == item, 'consequents'].iloc[0])
-    print((dfassoc.loc[rules['antecedents'] == item, 'confidence'].iloc[0]*100).round(2))
+    d = defaultdict(list)
+    count=(dfassoc.loc[dfassoc['antecedents'] == item, 'consequents']).count()
+   
+    ante=(dfassoc.loc[dfassoc['antecedents'] == item, 'consequents']).tolist()
+   
+    conf=((dfassoc.loc[dfassoc['antecedents'] == item, 'confidence']*100).round(2)).tolist()
+   
 
+    for i in range(count):
+        d[ante[i]].append(conf[i])
+    d=dict(d)
+
+    avgDict={}
+    for ante,conf in d.items():
+        avgDict[ante] = round((sum(conf)/ float(len(conf))),2)
+    print(avgDict)
 other_frequently_bought_item("JUMBO BAG PINK VINTAGE PAISLEY")
 
